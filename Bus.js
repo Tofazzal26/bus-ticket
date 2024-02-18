a1ff05e9b38b1624ac71e8dd895d83f47fa19bd0
Tofazzal26 <afranislamabir6789@gmail.com>
2024-02-18 15:17:32 +0600

    seat select function finish

--- Bus.js
+++ Bus.js
@@ -1,11 +1,36 @@
 
 
+const totalSeat = document.getElementsByClassName('Seat');
+
+for (  let index = 0; index < totalSeat.length; index++) {
+    const element = totalSeat[index];
+    element.addEventListener('click', function(event) {
+        const seatName = event.target.innerText
+        
+    
+    })
+}
 
 
+// way one
+
+// const transfer = document.getElementById('transfer');
+        
+// const p = document.createElement('p');
+        
+// const p3 = document.createElement('p');
+// const p2 = document.createElement('p');
+// p.innerText = seatName
+// p2.innerText = 'economy';
+// p3.innerText = 550;
+// transfer.appendChild(p);
+// transfer.appendChild(p2);
+// transfer.appendChild(p3);
 
 
-
-
-
+function commontIdText(id, value){
+    const element = document.getElementById(id)
+    element.innerText = value;
+}
 
 
